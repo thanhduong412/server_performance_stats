59806bad57f7f0333344cb4adf2d06595deb9a77
thanhduong412 <thanhduong412@gmail.com>
2025-05-28 16:57:53 +1000

    feat:syntax errors

--- server_stats.sh
+++ server_stats.sh
@@ -9,7 +9,7 @@ free -m | awk 'NR==2{printf"%sMB / %s MB (%.2f%%)\n", $3, $2, $3*100/$2}'
 # Total disk usage (Free vs Used including percentage)
 echo --------------------
 echo [INFO] Total Disk Usage:
-df -BM --total | awk '/total/ {used=$3,{printf "%s / %s (%s/ used)\n", $3, $2, $5}'
+df -BM --total | awk '/total/ {printf "%s / %s (%s/ used)\n", $3, $2, $5}'
 # Top 5 processes by CPU usage
 echo --------------------
 echo [INFO] Top 5 Processes by CPU Usage:
@@ -21,7 +21,7 @@ ps =eo pid,comm,$mem --sort=-%mem | head -n 6
 # OS version
 echo --------------------
 echo [INFO] OS Version:
-cat /etc/os-release
+cat /etc/os-release | grep "^PRETTY_NAME"
 # Uptime
 echo --------------------
 echo [INFO] Uptime:
